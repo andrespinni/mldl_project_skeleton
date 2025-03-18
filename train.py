2714d9052f600d898567316eba35eddf36ad4542
andrespinni <andrea.spinnicchia@gmail.com>
2025-03-18 17:42:14 +0100

    modified train

--- train.py
+++ train.py
@@ -1,1 +1,76 @@
 from models.customNet import CustomNet
+import torch
+from torch import nn
+
+train_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_train, batch_size=32, shuffle=True, num_workers=2)
+val_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_val, batch_size=32, shuffle=False, num_workers=2)
+
+def train(epoch, model, train_loader, criterion, optimizer):
+    model.train()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    for batch_idx, (inputs, targets) in enumerate(train_loader):
+        inputs, targets = inputs.cuda(), targets.cuda()
+
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+    train_loss = running_loss / len(train_loader)
+    train_accuracy = 100. * correct / total
+    print(f'Train Epoch: {epoch} Loss: {train_loss:.6f} Acc: {train_accuracy:.2f}%')
+
+# Validation loop
+def validate(model, val_loader, criterion):
+    model.eval()
+    val_loss = 0
+
+    correct, total = 0, 0
+
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(val_loader):
+            inputs, targets = inputs.cuda(), targets.cuda()
+
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            val_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+    val_loss = val_loss / len(val_loader)
+    val_accuracy = 100. * correct / total
+
+    print(f'Validation Loss: {val_loss:.6f} Acc: {val_accuracy:.2f}%')
+    return val_accuracy
+
+model = CustomNet().cuda()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+
+best_acc = 0
+
+# Run the training process for {num_epochs} epochs
+num_epochs = 10
+for epoch in range(1, num_epochs + 1):
+    train(epoch, model, train_loader, criterion, optimizer)
+
+    # At the end of each training iteration, perform a validation step
+    val_accuracy = validate(model, val_loader, criterion)
+
+    # Best validation accuracy
+    best_acc = max(best_acc, val_accuracy)
+
+
+print(f'Best validation accuracy: {best_acc:.2f}%')
